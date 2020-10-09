fcbdb892ad5ff5516400cfc769bde3070a0896db
hritu567 <hritumog96@gmail.com>
2020-10-10 01:51:05 +0530

    Added : Daily

--- employee.sh
+++ employee.sh
@@ -1,23 +1,74 @@
 #!/bin/bash -x
-isPartTime=1
-isFullTime=2
-totalSalary=0
-empRatePerHour=20
-numWorkingDays=20
-for (( day=1; day<=numWorkingDays; day++))
+
+
+IS_FULL_TIME=1;
+
+IS_PART_TIME=2;
+
+EMP_RATE_PER_HOUR=20;
+
+NUM_OF_WORKING_DAYS=20;
+
+MAX_HOURS_IN_MONTH=40;
+
+
+totalEmpHours=0;
+
+totalWorkingDays=0;
+
+
+function getWorkingHours()
+{
+	
+case $1 in 
+		
+       $IS_FULL_TIME )
+			
+       empHrs=8
+			
+       ;;
+		
+       $IS_PART_TIME )
+			
+        empHrs=4
+	
+        ;;
+	
+	* )
+	
+	empHrs=0
+
+	;;
+	
+esac
+
+	echo $empHrs
+}
+
+
+function getEmpWage()
+{
+	empHours=$1
+
+	echo $(($empHours*$EMP_RATE_PER_HOUR))
+}
+
+
+while [[ $totalEmpHours -lt $MAX_HOURS_IN_MONTH &&  $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
+
 do
-   empCheck=$((RANDOM%3))
-          case $empCheck in
-             $isFullTime)
-                  empHrs=8
-                   ;;
-             $isPartTime)
-                  empHrs=4
-                   ;;
-               *)
-                   empHrs=0
-                   ;;
-            esac
-        salary=$(($empHrs*$empRatePerHour))
-        totalSalary=$(($totalSalary*$salary))
+	
+     ((totalWorkingDays++))
+	
+     empHours=$( getWorkingHours $((RANDOM%3)) )	
+	
+     totalEmpHours=$(($totalEmpHours+$empHours))
+	
+      dailyWage[$totalWorkingDays]=$( getEmpWage $empHours )
+
 done
+
+
+  totalSalary=$(($totalEmpHours*$EMP_RATE_PER_HOUR));
+
+  echo "Daily Wages : " ${dailyWage[@]}
